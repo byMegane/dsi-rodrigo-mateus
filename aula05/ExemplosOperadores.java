b53aff2092c13eb05475d347eedcb9e93c94853e
Rodrigo Mateus <rodrigoglasses@gmail.com>
2026-04-27 09:22:14 -0300

    Exercicios e exemplos da aula 05

---
+++ aula05/ExemplosOperadores.java
@@ -0,0 +1,27 @@
+package aula05;
+
+public class ExemplosOperadores {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 2;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+
+        ++x;
+        System.out.println(x);
+        --x;
+        System.out.println(x);
+
+        int a = 100;
+        int b = 67;
+        System.out.println(a / b);
+          
+        double c = 100.0d;
+        double d = 67.0d;
+        System.out.println(c / d);
+    }
+}
