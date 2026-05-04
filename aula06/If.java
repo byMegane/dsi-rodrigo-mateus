1124e9bbd55fafd0029f7851525767e7bffabb8e
Rodrigo Mateus <rodrigoglasses@gmail.com>
2026-05-04 09:27:40 -0300

    começando o exercicio da aula06

---
+++ aula06/If.java
@@ -0,0 +1,12 @@
+package aula06;
+
+public class If {
+    public static void main(String[] args) {
+        int quantidadeDePessoas = 50;
+        boolean quantidadeLimite = true;
+
+        if () {
+            System.out.println("Bring an umbrella!");
+          }
+    }
+}
